66278c2c1dfdb49b2d666cd9c3ca37381ccc9c64
coldfire79 <joonyong.lee@pnnl.gov>
2020-06-19 09:05:36 -0700

    initial commit

---
+++ NetworkInference.spec
@@ -0,0 +1,24 @@
+/*
+A KBase module: NetworkInference
+*/
+
+module NetworkInference {
+
+    /* An X/Y/Z style reference */
+    typedef string obj_ref;
+
+    typedef structure {
+        obj_ref input_tbl;
+        string workspace_name;
+        string output_suffix;
+      } PCAParams;
+
+    typedef structure {
+        string report_name;
+        string report_ref;
+    } MIIAOutput;
+
+    /* run_miia: perform MIIA*/
+    funcdef run_miia(PCAParams params) returns (MIIAOutput output) authentication required;
+
+};
---
+++ lib/NetworkInference/NetworkInferenceImpl.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+#BEGIN_HEADER
+import logging
+import os
+
+from Methods.MIIA import MIIA
+from installed_clients.KBaseReportClient import KBaseReport
+#END_HEADER
+
+
+class NetworkInference:
+    '''
+    Module Name:
+    NetworkInference
+
+    Module Description:
+    A KBase module: NetworkInference
+    '''
+
+    ######## WARNING FOR GEVENT USERS ####### noqa
+    # Since asynchronous IO can lead to methods - even the same method -
+    # interrupting each other, you must be *very* careful when using global
+    # state. A method could easily clobber the state set by another while
+    # the latter method is running.
+    ######################################### noqa
+    VERSION = "0.0.1"
+    GIT_URL = ""
+    GIT_COMMIT_HASH = ""
+
+    #BEGIN_CLASS_HEADER
+    #END_CLASS_HEADER
+
+    # config contains contents of config file in a hash or None if it couldn't
+    # be found
+    def __init__(self, config):
+        #BEGIN_CONSTRUCTOR
+        self.config = config
+
+        self.callback_url = os.environ['SDK_CALLBACK_URL']
+        self.shared_folder = config['scratch']
+        logging.basicConfig(format='%(created)s %(levelname)s: %(message)s',
+                            level=logging.INFO)
+
+        self.miia_method = MIIA(self.config)
+
+        #END_CONSTRUCTOR
+        pass
+
+
+    def run_miia(self, ctx, params):
+        """
+        This example function accepts any number of parameters and returns results in a KBaseReport
+        :param params: instance of mapping from String to unspecified object
+        :returns: instance of type "ReportResults" -> structure: parameter
+           "report_name" of String, parameter "report_ref" of String
+        """
+        # ctx is the context object
+        # return variables are: output
+        #BEGIN run_miia
+        output = self.miia_method.run_miia(params)
+        #END run_miia
+
+        # At some point might do deeper type checking...
+        if not isinstance(output, dict):
+            raise ValueError('Method run_miia return value ' +
+                             'output is not type dict as required.')
+        # return the results
+        return [output]
+
+    def status(self, ctx):
+        #BEGIN_STATUS
+        returnVal = {'state': "OK",
+                     'message': "",
+                     'version': self.VERSION,
+                     'git_url': self.GIT_URL,
+                     'git_commit_hash': self.GIT_COMMIT_HASH}
+        #END_STATUS
+        return [returnVal]
